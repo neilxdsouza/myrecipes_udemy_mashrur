5c5381f63b612d6ebaf243e95553884e110ffb21
neil xavier dsouza <nxd_in@yahoo.com>
2015-12-01 12:25:43 +0530

    image uploads for recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -45,7 +45,8 @@ class RecipesController < ApplicationController
   private
 
     def recipe_params
-      params.require(:recipe).permit(:name, :summary, :description)
+      params.require(:recipe).permit(
+          :name, :summary, :description, :picture)
 
 
     end
